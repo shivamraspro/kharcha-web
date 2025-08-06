eedeb68c041094553a8d96baa92f8808482b9d20
Shivam Rastogi <shivamraspro@gmail.com>
2025-08-06 14:36:54 +0530

    chore: set port to 5050

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  devServer: {
+    port: 5050
+  },
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
 })
