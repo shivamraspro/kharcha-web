9fa8072fb01f92472a5bc5106b78cb5656cef64a
Shivam Rastogi <shivamraspro@gmail.com>
2025-08-15 21:38:09 +0530

    chore: setup eslint, stylistic and husky

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devServer: {
-    port: 5050
-  },
-  devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui'],
-})
+  devtools: { enabled: true },
+  devServer: {
+    port: 5050,
+  },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: {
+        // This is a factory config and can only be used to set some general styles
+        // View the source code to see how this factory config translates to eslint rules
+        // https://github.com/eslint-stylistic/eslint-stylistic/blob/main/packages/eslint-plugin/configs/customize.ts
+        // Full reference: https://eslint.style/rules/quote-props
+        arrowParens: true,
+        blockSpacing: true,
+        braceStyle: '1tbs',
+        commaDangle: 'always-multiline',
+        indent: 2,
+        jsx: true,
+        pluginName: '@stylistic',
+        quoteProps: 'consistent-as-needed',
+        quotes: 'single',
+        semi: true,
+        severity: 'warn',
+      },
+    },
+  },
+});
