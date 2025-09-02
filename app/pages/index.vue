525295ea144b6daa714ef3343c165d1fb4b5756f
Shivam Rastogi <shivamraspro@gmail.com>
2025-09-02 22:00:11 +0530

    chore: setup @nuxt/ui v4 and tailwindcss v4 (#18)

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const submitForm = () => {
+  console.log('form submitted');
+};
+</script>
+
+<template>
+  <div>
+    <div class="text-blue-200 text-4xl">
+      Hello World
+    </div>
+    <form
+      action=""
+      class="flex gap-2"
+    >
+      <input
+        type="text"
+        class="border px-4 py-2 rounded-md"
+      >
+      <button @click.prevent="submitForm">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
